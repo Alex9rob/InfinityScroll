84186d7609b1ea56cd03989592ff8e0c0a6f37c8
Oleksandr Drobyshev <oleksandrd@y-tree.com>
2020-08-12 11:53:55 +0300

    added gitignore

--- InfiniteScroll/InfiniteScroll/InfinityScroll/InfinityScroll.cs
+++ InfiniteScroll/InfiniteScroll/InfinityScroll/InfinityScroll.cs
@@ -30,6 +30,7 @@ namespace InfiniteScroll.InfinityScroll
             var localDataCount = localData.Count;
             if (_dataRequested)
             {
+
                 return;
             }
             switch (direction)
